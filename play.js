2ef78650cabf9ce826416d26a016e5138e341ab6
ChunyuBai <baichunyu26@gmail.com>
2022-11-23 13:44:16 -0500

    export the connect function import to play.js file

--- play.js
+++ play.js
@@ -1,20 +1,20 @@
-const net = require("net");
+// const net = require("net");
+const {connet} = require("./client");
+// // establishes a connection with the game server
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: "165.227.47.243",// IP address here,
+//     port: 50541// PORT number here,
+//   });
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",// IP address here,
-    port: 50541// PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log("hello! from client");
-  });
-  return conn;
-};
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
+//   conn.on("connect", () => {
+//     // code that does something when the connection is first established
+//     console.log("hello! from client");
+//   });
+//   return conn;
+// };
 
 console.log("Connecting ...");
-connect();
+// connect();
